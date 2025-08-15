5fd62d8dd9f8004df7619cb4ccfaa3ba7de14ec4
seed.suboty <seed.suboty@gmail.com>
2025-08-15 13:50:09 +0300

    refactor GNBG functions structure

---
+++ _gnbg_functions/move_to_dirs.sh
@@ -0,0 +1,16 @@
+#!/opt/homebrew/bin/bash
+
+ls mat || { echo "<mat> folder is not found!"; exit; }
+for filename in *.mat; do
+  mv "./$filename" "./mat/$filename"
+done
+
+ls txt || { echo "<txt> folder is not found!"; exit; }
+for filename in *.txt; do
+  mv "./$filename" "./txt/$filename"
+done
+
+ls json || { echo "<json> folder is not found!"; exit; }
+for filename in *.json; do
+  mv "./$filename" "./json/$filename"
+done
